5d6310e55af6ccd2cf325881855833d12816047b
John-PC <i.mitroshyn@synergetica.net>
2016-12-13 17:42:45 +0200

    ex_1

---
+++ exersise_1_infrastructure.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_example(driver):
+    driver.get("http://software-testing.ru/")
