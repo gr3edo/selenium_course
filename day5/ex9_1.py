277b0666fa9789b6cd10d4cdd4bf28b744cc5b25
Ivan <i.mitroshyn@synergetica.net>
2017-01-04 16:40:47 +0200

    ex9_1 pt1

---
+++ day5/ex9_1.py
@@ -0,0 +1,34 @@
+from telnetlib import EC
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_example(driver):
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    country = get_array_of_countries(driver)
+    sorted_country = sorted(country)
+
+    assert country == sorted_country
+
+    #develop timezones check
+
+def get_array_of_countries(driver):
+    item = driver.find_elements_by_xpath(".//*[@id='content']/form/table/tbody//td[5]/a")
+    country = []
+    for element in item:
+        country.append(element.text)
+    return country
