4fa797616a161e596a8414c17cca71596b10d89e
John-PC <i.mitroshyn@synergetica.net>
2017-01-04 22:32:07 +0200

    ex9_1 [3] - done (cleared unused imports)

--- day5/ex9_1.py
+++ day5/ex9_1.py
@@ -2,10 +2,6 @@ from telnetlib import EC
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-
 
 @pytest.fixture
 def driver(request):
@@ -24,8 +20,6 @@ def test_example(driver):
 
     assert (country == sorted_country)
 
-    # develop timezones check
-
     countries = driver.find_elements_by_xpath(".//*[@id='content']/form/table/tbody/tr[@class='row']")
     counter = 1
     for i in countries:
@@ -41,7 +35,6 @@ def test_example(driver):
             tz_countries_array.pop()
             sorted_tz_countries = sorted(tz_countries_array)
             assert (tz_countries_array == sorted_tz_countries)
-            print(sorted_tz_countries)
             driver.find_element_by_name("cancel").click()
 
 def get_array_of_countries(driver):
